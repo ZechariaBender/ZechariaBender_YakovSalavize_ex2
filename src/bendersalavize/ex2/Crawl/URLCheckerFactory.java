5f628549dcebe413f689b6a488b412f72a670248
ZechariaBender <zkbndr@gmail.com>
2019-05-01 14:13:04 +0300

    added documentation

--- src/bendersalavize/ex2/Crawl/URLCheckerFactory.java
+++ src/bendersalavize/ex2/Crawl/URLCheckerFactory.java
@@ -1,15 +1,19 @@
 package bendersalavize.ex2.Crawl;
 
-public class URLCheckerFactory {
+class URLCheckerFactory {
 
-    public URLChecker getChecker(String type) {
+    // getChecker returns new instance of specified URLChecker implementation
+    URLChecker getChecker(String type) {
+        // incidentally there is only one available implementation
         if (type.equalsIgnoreCase(ImageURLChecker.getKeyword())) {
             return new ImageURLChecker();
         }
         return null;
     }
 
-    public void printAll() {
+    // printAll() prints documentation for all available URLChecker implementations
+    void printAll() {
+        // incidentally there is only one available implementation
         ImageURLChecker.print();
     }
 }
